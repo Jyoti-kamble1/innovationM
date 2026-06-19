c623841401a0dd475127d224817246d3d8d8af7c
Jyoti-kamble1 <jyoti.kamble9828@gmail.com>
2026-06-19 13:32:05 +0530

    my commit

--- InnovationM/src/test/java/Demo/XYZ.java
+++ InnovationM/src/test/java/Demo/XYZ.java
@@ -3,5 +3,8 @@ package Demo;
 public class XYZ {
 	public static void main(String[] args) {
 		System.out.println("XYZ");
+		System.out.println("ABC");
+		System.out.println("ABC");
+		System.out.println("ABC");
 	}
 }
